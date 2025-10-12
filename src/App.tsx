0b2f4751670a717ed8d24defce8cdd43007d0062
Fazii04 <santyzambrana@gmail.com>
2025-10-12 19:58:13 -0400

    fw

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Esto es un cambio desde el</h2>
+      <h2>Esto es un cambio desde el vc.. hola yo viendo desde el celu</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
